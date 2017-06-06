e9cbcca152a2fffcae7cb29fb61d012a0b9878be
Sang Lee <s36lee@uwaterloo.ca>
2017-06-05 22:56:09 -0700

    Initial version

---
+++ augmentation.py
@@ -0,0 +1,76 @@
+import numpy
+import random
+import matplotlib.pyplot as plt
+import cv2
+import copy
+
+class AugBase:
+    def __init__(self):
+        pass
+
+    # Receives an image and angle, then
+    # return a new image with adjusted angle
+    def perform_aug(self, img, angle):
+        raise TypeError("Base class")
+
+class BlackAug(AugBase):
+    def __init__(self, length):
+        self.length = length
+
+    def perform_aug(self, img, angle):
+        img_w = img.shape[1]
+        left_right = random.randint(0, 10)
+
+        if (left_right >= 5):
+            start = 0
+            end = int(img_w * self.length)
+        else:
+            start = int(img_w * (1 - self.length))
+            end = img_w
+
+        img = copy.copy(img)
+        img[:, start:end, :] = 0
+
+        return img, angle
+
+class FlipAug(AugBase):
+    def perform_aug(self, img, angle):
+        img = numpy.fliplr(img)
+        angle = angle * -1
+
+        return img, angle
+
+class BrightnessAug(AugBase):
+    def __init__(self, max_delta):
+        self.max_delta = max_delta
+
+    def perform_aug(self, img, angle):
+        to_add = random.randint(-self.max_delta, self.max_delta)
+        img = numpy.add(img, int(to_add))
+        img = numpy.clip(img, 0, 255)
+        img = numpy.array(img, dtype=numpy.uint8)
+        return img, angle
+
+AUG_COLLECTION = [BlackAug(0.6), FlipAug(), BrightnessAug(20)]
+
+def behavior_perform_aug(aug_prob, img, angle):
+    for aug in AUG_COLLECTION:
+        if (random.randint(0, 10) > int(aug_prob * 20)):
+            img, angle = aug.perform_aug(img, angle)
+
+    return img, angle
+
+def unit_test(img, angle):
+    print(img.shape)
+    for aug in AUG_COLLECTION:
+        img, angle = aug.perform_aug(img, angle)
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        print("angle is %f", angle)
+        plt.show()
+
+def main():
+    img = cv2.imread('IMG/center_2016_12_01_13_30_48_287.jpg')
+    unit_test(img, 10)
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,163 @@
+import csv
+import psutil
+import sys
+import numpy
+import sklearn
+import cv2
+
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import keras.models
+from keras.layers import *
+from augmentation import *
+from keras.models import load_model
+
+BATCH_SIZE = 16
+RESIZED_SHAPE = (40, 80)
+
+def get_all_data(csv_loc = './driving_log.csv'):
+    to_return = []
+
+    with open(csv_loc) as csv_file:
+        reader = csv.reader(csv_file)
+        for line in reader:
+            to_return.append(line)
+
+    return to_return
+
+def show_image(image):
+    plt.imshow(image)
+    plt.show()
+
+def pretrain_image_process(image_path):
+    image = cv2.imread(image_path)
+    return image
+
+def center_generator(samples, batch_size, perform_aug):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        numpy.random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            img_index = 0
+
+            # center img loc, left img loc, right img loc, steer angle, throttle, break, speed
+            for batch_sample in batch_samples:
+                # Read as gray scale to begin with
+                image = pretrain_image_process(batch_sample[img_index])
+                angle = float(batch_sample[3])
+
+                if (image is None):
+                    continue
+
+                if (perform_aug == False) :
+                    # original image
+                    images.append(image)
+                    angles.append(angle)
+                else:
+                    image, angle = behavior_perform_aug(0.9, image, angle)
+                    images.append(image)
+                    angles.append(angle)
+
+            X_train = numpy.array(images)
+            y_train = numpy.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+def mean_normalize(x):
+    return K.mean(x/127.5 - 1., axis=3, keepdims=True)
+
+def resize_img(x):
+    # Need duplicative import due to a bug in kera's
+    # load_model
+    import keras.backend
+    return keras.backend.tf.image.resize_images(x, size=(40, 80))
+
+def train_get_initializer():
+    return keras.initializers.RandomNormal()
+
+def train_get_regularizer():
+    return keras.regularizers.l2(0.)
+
+def train_network(train_set, validation_set, image_shape, batch_size):
+    train_set_generator = center_generator(train_set, batch_size, True)
+    validation_set_generator = center_generator(validation_set, batch_size, False)
+
+    model = keras.models.Sequential()
+
+    # Start of Preprocess
+    # Grayscale AND normalize
+    model.add(Lambda(mean_normalize, input_shape=image_shape, output_shape=(image_shape[0], image_shape[1], 1)))
+
+    # Crop
+    crop_dimension=((65,15), (0,0))
+    model.add( Cropping2D(cropping=crop_dimension) )
+
+    # resize
+    resized_img_shape = (RESIZED_SHAPE[0], RESIZED_SHAPE[1], 1)
+    model.add(Lambda(resize_img, output_shape=resized_img_shape))
+    #end of pre process
+
+    # Conv section
+    model.add(Convolution2D(filters=6, kernel_size=5, strides=1, padding='valid', data_format="channels_last"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(filters=6, kernel_size=5, strides=1, padding='valid', data_format="channels_last"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(filters=6, kernel_size=5, strides=1, padding='valid', data_format="channels_last"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # end of Conv section
+
+    # Flatten
+    model.add(keras.layers.core.Flatten())
+    # End of flatten
+
+    # Start of Dense section
+    model.add(keras.layers.core.Dense(40, kernel_initializer=train_get_initializer(), \
+            kernel_regularizer=train_get_regularizer()))
+    model.add(keras.layers.Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(keras.layers.core.Dense(30, kernel_initializer=train_get_initializer(), \
+            kernel_regularizer=train_get_regularizer()))
+    model.add(keras.layers.Activation('linear'))
+
+    model.add(keras.layers.core.Dense(10, kernel_initializer=train_get_initializer(), \
+            kernel_regularizer=train_get_regularizer()))
+    model.add(keras.layers.Activation('linear'))
+
+    model.add(keras.layers.core.Dense(1))
+    model.add(keras.layers.Activation('linear'))
+    # End of Dense section
+
+    # Loss
+    model.compile(loss='mse', optimizer='adam')
+
+    # call backs
+    filepath="model_{epoch:02d}-{val_loss:.2f}.h"
+    callbacks = [ \
+        keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='min', period=1), \
+        keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=3, verbose=1, mode='min'), \
+        ]
+
+    # Training
+    model.fit_generator(train_set_generator, steps_per_epoch=len(train_set), \
+            validation_data=validation_set_generator, \
+            nb_val_samples=len(validation_set), nb_epoch=1000, callbacks=callbacks)
+
+    # Conclude
+    model.save('model.h5')
+
+def main():
+    samples = get_all_data()
+
+    train_samples, validation_samples = train_test_split(samples, test_size=0.05)
+    validation_samples = copy.deepcopy(train_samples)
+
+    image_shape = numpy.shape(pretrain_image_process(train_samples[0][0]))
+    train_network(train_samples, validation_samples, image_shape, BATCH_SIZE)
+
+if __name__ == '__main__':
+    main()
