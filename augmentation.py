5a0f0060a4092236f4017e52dca6daf0f19e0757
Sang Lee <s36lee@uwaterloo.ca>
2017-06-17 14:56:06 -0700

    augmentation: add Flip, Brightness, Shadow augmentation

--- augmentation.py
+++ augmentation.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import cv2
 import copy
+import math
 
 class AugBase:
     def __init__(self):
@@ -43,11 +44,15 @@ class FlipAug(AugBase):
 
 class BrightnessAug(AugBase):
     def __init__(self, max_delta):
-        self.max_delta = max_delta
+        self.max_delta = int(max_delta * 100)
 
     def perform_aug(self, img, angle):
-        to_add = random.randint(-self.max_delta, self.max_delta)
-        img = numpy.add(img, int(to_add))
+        start = 100 - self.max_delta
+        start /= 100.0
+        delta = random.randint(0, self.max_delta * 2)
+        delta /= 100.0
+
+        img = cv2.addWeighted(img, start, img, delta, 0.0)
         img = numpy.clip(img, 0, 255)
         img = numpy.array(img, dtype=numpy.uint8)
         return img, angle
@@ -63,19 +68,59 @@ class StretchAug(AugBase):
         img = img[img.shape[0] - org_shape[0]:img.shape[0], 0:img.shape[1]]
         return img, angle
 
-AUG_COLLECTION = [BlackAug(0.6), FlipAug()]
+class ShadowAug(AugBase):
+    def __init__(self, step_range):
+        self.step_range = step_range
+
+    def get_point(self, center_x, center_y, max_x, max_y):
+        x = None
+        y = None
+
+        while x is None or y is None:
+            x = random.randint(center_x - self.step_range, center_x + self.step_range)
+            y = random.randint(center_y - self.step_range, center_y + self.step_range)
+            # Instead of cropping, regenerate if over the range
+            # to avoid too many points being at the edge of the img
+
+            if x < 0 or x >= max_x:
+                x = None
+            if y < 0 or y >= max_y:
+                y = None
+
+        return (x, y)
+
+    def perform_aug(self, img, angle):
+        num_points = random.randint(3, 10)
+        points = []
+
+        max_x = img.shape[1]
+        center_x = random.randint(self.step_range, max_x - self.step_range)
+        max_y = img.shape[0]
+        center_y = random.randint(self.step_range, max_y - self.step_range)
+
+        for i in range(num_points - 1):
+            points.append(self.get_point(center_x, center_y, max_x, max_y))
+
+        points.sort()
+
+        shadow_img = copy.copy(img)
+        shadow_img = cv2.fillPoly(shadow_img, numpy.array([points]), (0,0,0))
+        img = cv2.addWeighted(img, 0.5, shadow_img, 0.5, 0.0)
+
+        return img, angle
+
+AUG_COLLECTION = [BlackAug(0.5), FlipAug(), ShadowAug(50), BrightnessAug(0.25), StretchAug(1.3)]
 
 def behavior_perform_aug(aug_prob, img, angle):
-    img, angle = AUG_COLLECTION[0].perform_aug(img, angle)
-    if ((aug_prob * 100) >= random.randint(0, 100)):
-        img, angle = AUG_COLLECTION[1].perform_aug(img, angle)
+    for aug in AUG_COLLECTION:
+        if ((aug_prob * 100) >= random.randint(0, 100)):
+            img, angle = aug.perform_aug(img, angle)
 
     return img, angle
 
-def unit_test(img, angle):
-    print(img.shape)
+def unit_test(org_img, angle):
     for aug in AUG_COLLECTION:
-        img, angle = aug.perform_aug(img, angle)
+        img, angle = aug.perform_aug(org_img, angle)
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         print("angle is %f", angle)
         plt.show()
